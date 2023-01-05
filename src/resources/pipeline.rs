c1b2130ee13539811010b6eca869a8983f9084a7
Matt DeBoer <matt.deboer@gmail.com>
2023-01-04 17:12:58 -0800

    fix pipeline_stop state name

--- src/resources/pipeline.rs
+++ src/resources/pipeline.rs
@@ -219,7 +219,7 @@ impl Pipeline {
     pub async fn stop(&self) -> Result<gstd_types::Response, Error> {
         let resp = self
             .client
-            .put(&format!("pipelines/{}/state?name=stop", self.name))
+            .put(&format!("pipelines/{}/state?name=null", self.name))
             .await?;
         self.client.process_resp(resp).await
     }
